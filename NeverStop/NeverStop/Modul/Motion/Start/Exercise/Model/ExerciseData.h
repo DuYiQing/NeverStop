bd581d3d6be3bec23df95e0c36879600ec547ac1
Jiang <suinian0531@163.com>
2016-10-28 09:18:54 +0800

    2016.10.28 09:18 进度更新

---
+++ NeverStop/NeverStop/Modul/Motion/Start/Exercise/Model/ExerciseData.h
@@ -0,0 +1,18 @@
+//
+//  ExerciseData.h
+//  NeverStop
+//
+//  Created by Jiang on 16/10/27.
+//  Copyright © 2016年 JDT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExerciseData : NSObject
+@property (nonatomic, assign) CGFloat distance;
+@property (nonatomic, assign) NSInteger duration;
+@property (nonatomic, assign) CGFloat speedPerHour;
+@property (nonatomic, assign) CGFloat averageSpeed;
+@property (nonatomic, assign) CGFloat maxSpeed;
+@property (nonatomic, assign) CGFloat calorie;
+@end

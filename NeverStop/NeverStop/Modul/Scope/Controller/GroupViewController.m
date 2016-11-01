b1a1e07a6c10658204e3e23849f46cde3d09cd27
CCCh <18842663400@163.com>
2016-11-01 19:13:25 +0800

    2016.11.1 做了啥?运动圈二级界面底下的色块

--- NeverStop/NeverStop/Modul/Scope/Controller/GroupViewController.m
+++ NeverStop/NeverStop/Modul/Scope/Controller/GroupViewController.m
@@ -17,7 +17,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor blackColor];
+    UIImageView *imageView = [[UIImageView alloc] init];
+    imageView.frame = CGRectMake(0, 0, SCREEN_WIDTH, 200);
+    imageView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:imageView];
+    
+    
+    UIImageView *userImageView = [[UIImageView alloc] init];
+    userImageView.frame = CGRectMake(30, 150, 100, 100);
+    userImageView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:userImageView];
+    
+    
+    UILabel *titleLabel = [[UILabel alloc] init];
+    titleLabel.frame = CGRectMake(30, 270, 300,40);
+    titleLabel.backgroundColor = [UIColor brownColor];
+    titleLabel.text = @"121";
+    titleLabel.textColor = [UIColor orangeColor];
+    [self.view addSubview:titleLabel];
+    
+    
+    UILabel *userContLabel = [[UILabel alloc] init];
+    userContLabel.frame = CGRectMake(40, 310, 40, 20);
+    userContLabel.backgroundColor = [UIColor blueColor];
+    userContLabel.text = @"aaa";
+    userContLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:userContLabel];
+    
+    UILabel *addressLabel = [[UILabel alloc] init];
+    addressLabel.frame = CGRectMake(90, 310, 40, 20);
+    addressLabel.backgroundColor = [UIColor blueColor];
+    addressLabel.text = @"aasd";
+    addressLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:addressLabel];
+
+    
+    UIButton *goButton = [[UIButton alloc] init];
+    goButton.frame = CGRectMake(SCREEN_WIDTH - 100, 240, 100, 40);
+    goButton.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:goButton];
+    
+    
+    
+    UIButton *gogoButton = [[UIButton alloc] init];
+    gogoButton.frame = CGRectMake(50, SCREEN_HEIGHT - 100, SCREEN_WIDTH - 100, 40);
+    gogoButton.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:gogoButton];
+    
     
     
     
